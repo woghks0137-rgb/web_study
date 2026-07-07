390863a70fdbba698dab085fbd0e9e02d06d77f0
jaehwan <woghks0137@gmail.com>
2026-07-07 13:53:09 +0900

    html/css (js) 연습

--- 반응형웹2/greenhabit/js/script.js
+++ 반응형웹2/greenhabit/js/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
-
+    //추천상품 swiper 
+    var swiper = new Swiper('.recom-swiper', {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
 })
